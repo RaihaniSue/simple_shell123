96c09ead6e32dbeb1b1f8a2d1497674c1bbc4222
RaihaniSue <sue.raih@gmail.com>
2023-11-10 23:36:56 -0800

    The program  function executes a predefined built-in function corresponding to a given shell command or returns -1 if no match is found.

--- builtins_list.c
+++ builtins_list.c
@@ -1,9 +1,8 @@
 #include "shell.h"
 /**
- * builtins_list - search for match and execute the associate builtin
- * @data: struct for program data.
- * Return: Returns of the function executed if there is a match,
- * otherwise returns -1.
+ * builtins_list - entry point
+ * @data: data struct
+ * Return: the executed functions, -1 otherwise
  **/
 int builtins_list(data_of_program *data)
 {
@@ -19,16 +18,15 @@ int builtins_list(data_of_program *data)
 		{NULL, NULL}
 	};
 
-/*walk through the structure*/
+/* in between structure*/
 	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
 	{
-/*if there is a match between the given command and a builtin,*/
 		if (str_compare(options[iterator].builtin, data->command_name, 0))
 		{
-/*execute the function, and return the return value of the function*/
+/*execute the function and return*/
 			return (options[iterator].function(data));
 		}
-/*if there is no match return -1 */
+/*no match return -1 */
 	}
 	return (-1);
 }
